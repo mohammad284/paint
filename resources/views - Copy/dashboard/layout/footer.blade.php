80872b63d68e6e1d9b830eae95f40d61ce93b6b6
mohammad284 <122008647+mohammad284@users.noreply.github.com>
2023-12-26 12:45:50 +0300

    Delete resources/views - Copy directory

--- resources/views - Copy/dashboard/layout/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-  <p class="text-muted mb-1 mb-md-0">malar .  </p>
-  <p class="text-muted">made by <a href="#" target="_blank">smart</a> <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
-</footer>
--- resources/views - Copy/emails/reply.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{__('email from support')}}</title>
-</head>
-<body>
-    <p> {{ $mail['message'] }} </p>
-</body>
-</html> 
